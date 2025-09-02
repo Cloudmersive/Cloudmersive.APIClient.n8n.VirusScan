2552f213436caccc1f54eb4cbcca86a7ff0e11db
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2025-09-02 07:05:37 +0000

    Bug fix

--- n8n-nodes-cloudmersive-virus-scan-api/nodes/CloudmersiveVirusScanApi/CloudmersiveVirusScanApi.node.ts
+++ n8n-nodes-cloudmersive-virus-scan-api/nodes/CloudmersiveVirusScanApi/CloudmersiveVirusScanApi.node.ts
@@ -19,7 +19,7 @@ export class CloudmersiveVirusScanApi implements INodeType {
 	description: INodeTypeDescription = {
 		displayName: 'Cloudmersive Virus Scan',
 		name: 'cloudmersiveVirusScanApi',
-		icon: 'file:cloudmersive.svg',
+		icon: 'file:cloudmersive.png',
 		group: ['transform'],
 		version: 1,
 		description: 'Scan files, websites, and cloud storage for malware via Cloudmersive',
